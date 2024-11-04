5cd12194f1811f5f2277c57d1aa5993ae964fc92
NakedGreen <jokemup@gmail.com>
2024-11-04 15:44:36 +0900

    [feat] demo.py update

--- demo.py
+++ demo.py
@@ -10,36 +10,74 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-MAPPING_EXAMPLE = {
+MAPPING = {
   '마포': './Minseok/res/reviews.json', 
   '서대문': './Minseok/res/shillastay_Seodaemun.json',
   '역삼': './Minseok/res/shillastay_Yeoksam.json',
 }
-
-# Pickle file open example
-# with open('./Minseok/res/prompt_1shot.pickle', 'rb') as f:
-#     PROMPT = pickle.load(f)
+with open('./Minseok/res/prompt_1shot.pickle', 'rb') as f:
+    PROMPT = pickle.load(f)
 
 
 
 def preprocess_reviews(path='./res/reviews.json'):
-    # 모델 실습의 입력 데이터가 고도화된 전처리 함수와 동일합니다. 
-    return 
+    with open(path, 'r', encoding='utf-8') as f:
+        review_list = json.load(f)
+
+    reviews_good, reviews_bad = [], []
+
+    current_date = datetime.datetime.now()
+    date_boundary = current_date - datetime.timedelta(days=6*30)
+
+    filtered_cnt = 0
+    for r in review_list:
+        review_date_str = r['date']
+        try:
+            review_date = parser.parse(review_date_str)
+        except (ValueError, TypeError):
+            review_date = current_date
+
+        if review_date < date_boundary:
+            continue
+        if len(r['review']) < 30:
+            filtered_cnt += 1
+            continue
+
+        if r['stars'] == 5:
+            reviews_good.append('[REVIEW_START]' + r['review'] + '[REVIEW_END]')
+        else:
+            reviews_bad.append('[REVIEW_START]' + r['review'] + '[REVIEW_END]')
+
+    reviews_good = reviews_good[:min(len(reviews_good), 50)]
+    reviews_bad = reviews_bad[:min(len(reviews_bad), 50)]
+
+    reviews_good_text = '\n'.join(reviews_good)
+    reviews_bad_text = '\n'.join(reviews_bad)
+
+    return reviews_good_text, reviews_bad_text
 
 
 def summarize(reviews):
-    # 1. prompt를 불러와서 review를 합쳐줍니다. 
-    # 2. OpenAI API를 불러와서 prompt를 넣어줍니다. 
-    # 3. 결과를 반환합니다.
-    return 
+
+    prompt = PROMPT + '\n\n' + reviews
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    completion = client.chat.completions.create(
+        model='gpt-3.5-turbo-0125',
+        messages=[{'role': 'user', 'content': prompt}],
+        temperature=0.0
+    )
+    return completion
 
 
 def fn(accom_name):
-    # 1. MAPPING을 통하여 파일 경로를 받아옵니다. 
-    # 2. preprocess_reviews를 통하여 리뷰를 받아옵니다.
-    # 3. summarize를 통하여 리뷰를 요약합니다.
-    # 4. 요약된 리뷰를 반환합니다.
-    return 
+    path = MAPPING[accom_name]
+    reviews_good, reviews_bad = preprocess_reviews(path)
+
+    summary_good = summarize(reviews_good).choices[0].message.content
+    summary_bad = summarize(reviews_bad).choices[0].message.content
+
+    return summary_good, summary_bad
+
 
 def run_demo():
   demo = gr.Interface(
